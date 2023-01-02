784390b12ab93d9173ef6daf48fc60fa9da37991
Ryan Chung <msc2pd@virginia.edu>
2023-01-02 18:08:15 -0500

    :wq ~#

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -33,7 +33,7 @@ const MyProjects = () => {
     },
   ]
   return (
-    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1}} transition={{ duration: 1 }} name="Projects" className="w-full text-white md:h-screen">
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1}} transition={{ duration: 1 }} name="Projects" className="w-full text-white sm:h-screen">
         <div className="max-w-screen-lg px-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8 text-center md:text-left">
           <p className="text-md tracking-widest inline text-zinc-700">
